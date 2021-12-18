f04d1b49dae1a41ac0c81334411f826d6a21b6ad
mizuky fujitani <mizuki.fujitani@gmail.com>
2021-12-18 22:11:13 +0900

    "Assets, Metadata, and CSS" P6の "Adding CSS" までの変更

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,9 +1,11 @@
+import styles from './layout.module.css'
+
 const Layout = ({
     children
 }: {
     children: React.ReactNode
 }) => {
-    return <div>{children}</div>
+    return <div className={styles.container}>{children}</div>
 }
 
 export default Layout
