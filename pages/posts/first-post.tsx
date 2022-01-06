ec357633750952c3fcada4285ff5601f2872db05
mizuki.fujitani <mizuki.fujitani@persol.co.jp>
2022-01-06 15:55:57 +0900

    "Navigate Between Pages" P4の変更

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 
-const FirstPost : NextPage = () => {
-    return <h1>First Post</h1>
+const FirstPost: NextPage = () => {
+    return (
+        <>
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">
+                    <a>Back to home</a>
+                </Link>
+            </h2>
+        </>
+    )
 }
 
 export default FirstPost
