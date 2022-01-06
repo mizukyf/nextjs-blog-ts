bbe4a91e3129de2217cde9bd85832e5a20ee3a09
mizuki.fujitani <mizuki.fujitani@persol.co.jp>
2022-01-06 16:38:42 +0900

    "Dynamic Routes" P7の "Adding CSS" までの変更

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,6 +3,7 @@ import Layout from '../../components/layout'
 import Date from '../../components/date'
 import Head from 'next/head'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
 
 type PostProps = {
     postData: {
@@ -19,14 +20,13 @@ const Post: NextPage<PostProps> = ({ postData }) => {
             <Head>
                 <title>{postData.title}</title>
             </Head>
-
-            {postData.title}
-            <br />
-            {postData.id}
-            <br />
-            <Date dateString={postData.date} />
-            <br />
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>
     )
 }
